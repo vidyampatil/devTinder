26bdbdedccd236005bbe2da0852d900c810e2c6c
vidyampatil <vidyapatil11096@gmail.com>
2026-04-03 10:01:21 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,11 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res)=>{
-    res.send("hello from server")
+//This will only handle Get call to user
+app.get("/user",(req,res)=>{
+    res.send({firstName:'vidya',lastName:'Patil'});
 })
 
-app.use("/hello",(req,res)=>{
+app.post("/user",(req,res)=>{
+    res.send("Data successfully save to database")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data successfully deleted from database")
+})
+
+//This will match all the HTTP methods API calls to test
+app.use("/test",(req,res)=>{
     res.send("hello from server2");
 })
 
